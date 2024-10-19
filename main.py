9d5ffaea4f17b09abed65f67394f13eb4df53230
Sri Charan Thoutam <thoutamsricharan@gmail.com>
2024-10-19 20:28:18 +0530

    Data Validation Stage Completed

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from mlProject import logger
 from mlProject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from mlProject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 logger.info("Welcome to Diamond Price Prediction Project!")
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
-    logger.info(f"\n>>>>>> {STAGE_NAME} started <<<<<<\n")
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f"\n>>>>>> {STAGE_NAME} completed <<<<<<\n")
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
 
 except Exception as e:
     logger.exception(e)
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import DataIngestionConfig
+from mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     # pull yaml file paths from constants
@@ -33,3 +33,25 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        # config file
+        config = self.config.data_validation
+
+        # schema file
+        schema = self.schema.COLUMNS
+
+        # create data validation directory
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            # config
+            root_dir = config.root_dir,
+            unzip_data_dir = config.unzip_data_dir,
+            STATUS_FILE = config.STATUS_FILE,
+
+            # schema
+            all_schema = schema
+        )
+        
+        return data_validation_config
