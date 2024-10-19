e7d08742f1b9f37a2174d91cbf690065c6a80296
Sri Charan Thoutam <thoutamsricharan@gmail.com>
2024-10-20 01:27:59 +0530

    Data Transformation Stage Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 from mlProject.pipeline.data_validation_pipeline import DataValidationPipeline
+from mlProject.pipeline.data_transformation_pipeline import DataTransformationPipeline
 
 logger.info("Welcome to Diamond Price Prediction Project!")
 
@@ -22,6 +23,18 @@ try:
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationPipeline()
     data_validation.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
 
 except Exception as e:
